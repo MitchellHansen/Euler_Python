6d6ae816ebc13080078c158d11e27a47e9222e5c
MitchellHansen <mitchellhansen0@gmail.com>
2016-01-21 23:46:58 -0800

    Initial commit  Changes to be committed: 	new file:   problem_1.py         new file:   problem_2.py 	new file:   problem_3.py

---
+++ problem_1.py
@@ -0,0 +1,13 @@
+import math
+
+
+sum = 0
+
+
+for x in range(0, 1000):
+    if x % 3 == 0:
+        sum += x
+    elif x % 5 == 0:
+        sum += x
+
+print(sum)
---
+++ problem_2.py
@@ -0,0 +1,19 @@
+
+prev_two = 1;
+prev_one = 0;
+fib_num = 0
+sum = 0
+
+while True:
+    if fib_num > 4000000:
+        break
+
+    fib_num = prev_one + prev_two
+
+    if fib_num % 2 == 0:
+        sum += fib_num
+
+    prev_two = prev_one
+    prev_one = fib_num
+
+print(sum)
